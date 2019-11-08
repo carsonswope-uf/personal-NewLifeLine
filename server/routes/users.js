ff94c2c54f90c67e200854192c04c3eec0586dc4
cdiego55 <covielladiego@ufl.edu>
2019-11-08 14:47:03 -0500

    added some validators

--- server/routes/users.js
+++ server/routes/users.js
@@ -11,27 +11,58 @@ router.get('/login', function(req, res){
     res.send({type: 'login'});
 });
 
-router.post('/register', function(req, res){
+router.post('/register', async function(req, res){
+    var add = true;
+
     const name = req.body.name;
     const email = req.body.email;
     const username = req.body.username;
     const password = req.body.password;
+    const confirmPassword = req.body.confirmPassword;
 
-    let newUser = new User({
-        name:name,
-        email:email,
-        username:username,
-        password:password
-    });
-
-    newUser.save(function(err){
-        if(err){
-          console.log(err);
-          return;
-        } else {
-          res.redirect('/users/login');
+    let query = {username:username};
+    await User.findOne(query, function(err, user){
+        console.log(query);
+        if (user) {
+            add = false;
+            console.log("Username already exists");
         }
     });
+
+    let query2 = {email:email};
+    await User.findOne(query2, function(err, email){
+        if (email) {
+            add = false;
+            console.log("Email already exists");
+        }
+    });
+
+    if (password !== confirmPassword) {
+        add = false;
+        console.log("Passwords do not match");
+    }
+
+    console.log(add);
+
+    if(add) {
+        let newUser = new User({
+            name:name,
+            email:email,
+            username:username,
+            password:password
+        });
+    
+        newUser.save(function(err){
+            if(err){
+              console.log(err);
+              return;
+            } else {
+              res.redirect('/users/login');
+            }
+        });
+    } else {
+        res.send({message: "Not added"});
+    }
 });
 
 router.post('/login', function(req,res){
