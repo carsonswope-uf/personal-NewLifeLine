8d75849d632c6953933fca483d746b31f583abf2
cdiego55 <covielladiego@ufl.edu>
2019-11-08 15:54:40 -0500

    added email and password validators

--- server/routes/users.js
+++ server/routes/users.js
@@ -12,6 +12,16 @@ router.get('/login', function(req, res){
 });
 
 router.post('/register', async function(req, res){
+    function validateEmail(email) {
+        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(String(email).toLowerCase());
+    }
+
+    function validatePassword(password) {
+        var re = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{6,20}$/;
+        return re.test(password);
+    }
+
     var add = true;
 
     const name = req.body.name;
@@ -22,47 +32,58 @@ router.post('/register', async function(req, res){
 
     let query = {username:username};
     await User.findOne(query, function(err, user){
-        console.log(query);
         if (user) {
             add = false;
-            console.log("Username already exists");
+            res.write("Username already exists\n");
         }
     });
+
+    if (!validateEmail(email)) {
+        add = false;
+        res.write("Not a valid email\n");
+    }
 
     let query2 = {email:email};
     await User.findOne(query2, function(err, email){
         if (email) {
             add = false;
-            console.log("Email already exists");
+            res.write("An account with that email already exists\n");
         }
     });
 
+    if (!validatePassword(password)) {
+        add = false;
+        res.write("Pass word must be 6 - 20 characters and contain at least:\n -one lowercase letter\n -one uppercase letter\n -one numeric digit\n -one special character\n");
+    }
+
     if (password !== confirmPassword) {
         add = false;
-        console.log("Passwords do not match");
+        res.write("Passwords do not match\n");
     }
 
-    console.log(add);
-
-    if(add) {
-        let newUser = new User({
-            name:name,
-            email:email,
-            username:username,
-            password:password
-        });
+    await setTimeout(() => {
+        if(add) {
+            let newUser = new User({
+                name:name,
+                email:email,
+                username:username,
+                password:password
+            });
+        
+            newUser.save(function(err){
+                if(err){
+                  console.log(err);
+                  return;
+                } else {
+                  res.redirect('/users/login');
+                }
+            });
+        } else {
+            res.write("Not added");
+            res.end();
+        }        
+    }, 500);
     
-        newUser.save(function(err){
-            if(err){
-              console.log(err);
-              return;
-            } else {
-              res.redirect('/users/login');
-            }
-        });
-    } else {
-        res.send({message: "Not added"});
-    }
 });
 
 router.post('/login', function(req,res){
